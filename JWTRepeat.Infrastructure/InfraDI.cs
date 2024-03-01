6a3537a0e7ad942de2b5034b8d6ce6aef5b42acf
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-01 07:04:13 +0500

    infra yaratildi

---
+++ JWTRepeat.Infrastructure/InfraDI.cs
@@ -0,0 +1,28 @@
+﻿using JWTRepeat.Application.Abstractions;
+using JWTRepeat.Infrastructure.Persistance;
+using JWTRepeat.Infrastructure.Repositories.BaseRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTRepeat.Infrastructure
+{
+    public static class InfraDI
+    {
+        public static IServiceCollection AddJWTContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<JWTRepeatDbContext>(ops =>
+            {
+                ops.UseNpgsql(configuration.GetConnectionString(""));
+            });
+            services.AddScoped<IUserRepository, UserRepository>();
+            return services;
+
+        }
+    }
+}
---
+++ JWTRepeat.Infrastructure/Persistance/JWTRepeatDbContext.cs
@@ -0,0 +1,21 @@
+﻿using JWTRepeat.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTRepeat.Infrastructure.Persistance
+{
+    public class JWTRepeatDbContext : DbContext
+    {
+        public JWTRepeatDbContext(DbContextOptions<JWTRepeatDbContext> ops)
+            : base(ops)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
