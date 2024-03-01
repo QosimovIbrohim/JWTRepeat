da8159d6ce728d98090acb74897467525e652ad0
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-01 07:04:51 +0500

    Register and clean API

--- JWTRepeat.API/Program.cs
+++ JWTRepeat.API/Program.cs
@@ -1,3 +1,4 @@
+using JWTRepeat.Infrastructure;
 
 namespace JWTRepeat.API
 {
@@ -13,7 +14,7 @@ namespace JWTRepeat.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddJWTContext(builder.Configuration);
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
